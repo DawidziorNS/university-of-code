10127b412dbcd8dfece6f3f0c8582197106b9131
dawid <dawidjasinski@my.com>
2023-04-04 15:11:36 +0200

    New tasks

--- tasks/2023-04/03-04-2023-vowel-counter/task.js
+++ tasks/2023-04/03-04-2023-vowel-counter/task.js
@@ -1,5 +1,18 @@
 const countVowels = (word) => [...word.toLowerCase()].reduce((acc, item) => ['a', 'e', 'o', 'u', 'i'].includes(item) ? acc + 1 : acc, 0)
 
+const countVowels2 = (word) => word.split('').filter((x) => 'aeiouAEIOU'.includes(x)).length
+
+const countVowels3 = (word) => [...word].filter((x) => 'aeiouAEIOU'.includes(x)).length
+
+
 console.log(countVowels('Sonny'))
 console.log(countVowels('PAPAFAM'))
 console.log(countVowels('Prediction'))
+
+console.log(countVowels2('Sonny'))
+console.log(countVowels2('PAPAFAM'))
+console.log(countVowels2('Prediction'))
+
+console.log(countVowels3('Sonny'))
+console.log(countVowels3('PAPAFAM'))
+console.log(countVowels3('Prediction'))
---
+++ tasks/2023-04/04-04-2023-return-the-add-function/task.js
@@ -0,0 +1,5 @@
+const add = (n1) => (n2) => n1 + n2
+
+console.log(add(10)(20))
+console.log(add(0)(20))
+console.log(add(-30)(80))
